3c9871cad14edc51cd172fbc9e419ce02599b691
Shrijal Pradhan <pradhan.shrijal@gmail.com>
2024-03-11 10:13:56 +0100

    Update to RUN latest image

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-# Usage: source run.sh ${DOC_IMGAINER_NAME}
+# Usage: source run.sh ${DOC_IMAGE_NAME}
 DOC_IMG_NAME=$1
 CONT_NAME=$2
+ENV_VERSION=$3
 DOC_IMG_NAME="${DOC_IMG_NAME:=pha-22}"
 CONT_NAME="${CONT_NAME:=${DOC_IMG_NAME}}"
-ENV_VERSION=$(cat envs/$DOC_IMG_NAME/ENV_VERSION)
+ENV_VERSION="${ENV_VERSION:=latest}"
+# ENV_VERSION="${ENV_VERSION:=$(cat envs/$DOC_IMG_NAME/ENV_VERSION)}"
 
-docker run -d --name ${CONT_NAME} -e DISPLAY=$DISPLAY --env=NVIDIA_VISIBLE_DEVICES=all --env=NVIDIA_DRIVER_CAPABILITIES=all  --env=QT_X11_NO_MITSHM=1 --runtime=nvidia --privileged --shm-size=16gb -v /tmp/.X11-unix:/tmp/.X11-unix --network host -v /home/${USER}/schreibtisch/pha_docker_files/docker_share:/home/pha/docker_share -v /media/${USER}:/media/pha -v /dev:/dev --gpus all -it phaenvs/${DOC_IMG_NAME}:${ENV_VERSION} /bin/bash
+docker run -d --name ${CONT_NAME} \
+                -e DISPLAY=$DISPLAY \
+                --env=NVIDIA_VISIBLE_DEVICES=all \
+                --env=NVIDIA_DRIVER_CAPABILITIES=all \
+                --env=QT_X11_NO_MITSHM=1 \
+                --runtime=nvidia \
+                --privileged \
+                --shm-size=16gb \
+                -v /tmp/.X11-unix:/tmp/.X11-unix \
+                --network host \
+                -v /home/${USER}/schreibtisch/pha_docker_files/docker_share:/home/pha/docker_share \
+                -v /media/${USER}:/media/pha \
+                -v /dev:/dev \
+                --gpus all \
+                -it phaenvs/${DOC_IMG_NAME}:${ENV_VERSION} /bin/bash
