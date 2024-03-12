399d25d7171ad69d1d100ef985aa58f082234c05
Shrijal Pradhan <pradhan.shrijal@gmail.com>
2024-03-12 23:58:33 +0100

    Inclusion of TensorRT

---
+++ docker_share/scripts/install/install_tensorrt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+IN_UBUNTU=$1
+IN_TRT=$2
+IN_CUDA=$3
+IN_TRT="${IN_TRT:=8.6.1}"
+IN_CUDA="${IN_CUDA:=11.8}"
+IN_UBUNTU="${IN_UBUNTU:=2204}"
+
+wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/secure/${IN_TRT}/local_repos/nv-tensorrt-local-repo-ubuntu${IN_UBUNTU}-${IN_TRT}-cuda-${IN_CUDA}_1.0-1_amd64.deb
+sudo dpkg -i nv-tensorrt-local-repo-ubuntu${IN_UBUNTU}-${IN_TRT}-cuda-${IN_CUDA}_1.0-1_amd64.deb
+sudo cp /var/nv-tensorrt-local-repo-ubuntu${IN_UBUNTU}-${IN_TRT}-cuda-${IN_CUDA}/nv-tensorrt-local-3E951519-keyring.gpg /usr/share/keyrings/
+sudo apt update
+sudo apt install tensorrt -y
+sudo apt install python3-libnvinfer-dev -y
+python3 -m pip install protobuf
+sudo apt install uff-converter-tf -y
+python3 -m pip install onnx
+sudo apt install onnx-graphsurgeon -y
+rm -rf nv-tensorrt-local-repo-ubuntu${IN_UBUNTU}-${IN_TRT}-cuda-${IN_CUDA}_1.0-1_amd64.deb
