a76ef25b052727317c2219c10461bf18224a3b0e
Shrijal Pradhan <pradhan.shrijal@gmail.com>
2024-03-26 18:15:55 +0100

    Compose with passed user

--- docker_scripts/run-compose.sh
+++ docker_scripts/run-compose.sh
@@ -4,11 +4,19 @@ ENV_FILE="docker_scripts/compose-file.env"
 DOC_FILE="docker_scripts/docker-compose.yaml"
 DOC_COMMAND="up -d"
 
-while getopts e:f:o: flag
+UID_VAR=$(id -u)
+GID_VAR=$(id -g)
+
+export UID_VAR=${UID_VAR}
+export GID_VAR=${GID_VAR}
+
+while getopts d:e:f:g:o: flag
 do
     case "${flag}" in
+        d) UID_VAR=${OPTARG};;
         e) ENV_FILE=${OPTARG};;
         f) DOC_FILE=${OPTARG};;
+        g) GID_VAR=${OPTARG};;
         o) DOC_COMMAND=${OPTARG};;
         *) error "Unexpected option ${flag}" ;;
     esac
