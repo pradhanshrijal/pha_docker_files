506d81c62ebb1f04f5ab8c85503e1a9afe9df6de
Shrijal Pradhan <shrijal.pradhan.company.man@gmail.com>
2024-05-26 11:32:58 +0200

    Sample Scripts Sourcing for jekyll-nano

---
+++ docker_share/scripts/setup/export_pha.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PHA_HOME=/home/prad_sh/schreibtisch/pha_docker_files
+export SSI_PATH=${PHA_HOME}/docker_share
+export ADDONS_PHA=${SSI_PATH}/git_pkgs/pha_addons
+export SIMULATORS_PHA=${SSI_PATH}/git_pkgs/simulators
--- docker_share/scripts/setup/setup_ssi.sh
+++ docker_share/scripts/setup/setup_ssi.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-cd /home/${USER}
-mkdir schreibtisch
-cd schreibtisch
-git clone https://github.com/pradhanshrijal/pha_docker_files --recursive
+PHA_PARENT=/home/${USER}/schreibtisch
+
+if [ ! -d "$PHA_PARENT" ]; then
+  mkdir -p $PHA_PARENT
+fi
+
+cd $PHA_PARENT
+
+if [ ! -d pha_docker_files ]; then
+  git clone https://github.com/pradhanshrijal/pha_docker_files --recursive
+fi
+
 cd pha_docker_files
+
+if [[ -z "${PHA_HOME}" ]]; then
+    echo "source ${PHA_PARENT}/pha_docker_files/docker_share/scripts/setup/export_pha.sh" >> /home/${USER}/.bashrc
+fi
