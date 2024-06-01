bbfaaa3f72dff7b0d15514e8e4a6bb683f7e2c80
Shrijal Pradhan <shrijal.pradhan.company.man@gmail.com>
2024-06-01 08:20:44 +0200

    Final Changes

--- docker_share/scripts/setup/setup_ssi.sh
+++ docker_share/scripts/setup/setup_ssi.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-PHA_PARENT=/home/${USER}/schreibtisch
+# Setup Carlaware
+# Sample: source setup_cwr.sh /home/pha/schreibtisch
 
+# Variables
+PHA_PARENT=$1
+PHA_PARENT="${PHA_PARENT:=/home/${USER}/schreibtisch}"
+#
+
+# Install Requirements
+sudo apt-get install python3 python3-pip -y
+python3 -m pip install gdown
+#
+
+# Parent Folder
 if [ ! -d "$PHA_PARENT" ]; then
   mkdir -p $PHA_PARENT
 fi
+#
 
+# PHA Folder
 cd $PHA_PARENT
 
 if [ ! -d pha_docker_files ]; then
@@ -15,6 +29,7 @@ fi
 cd pha_docker_files
 
 if [[ -z "${PHA_HOME}" ]]; then
-    echo "# PHA" >> /home/${USER}/.bashrc
-    echo "source ${PHA_PARENT}/pha_docker_files/docker_share/scripts/setup/export_pha.sh" >> /home/${USER}/.bashrc
+  echo "# PHA" >> /home/${USER}/.bashrc
+  echo "source ${PHA_PARENT}/pha_docker_files/docker_share/scripts/setup/export_pha.sh" >> /home/${USER}/.bashrc
 fi
+#
