b8703ef13e6206bb74b7176da1a916d5d4a9e47d
Shrijal Pradhan <shrijal.pradhan.company.man@gmail.com>
2024-06-17 14:39:51 +0200

    Integrate User Switch

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 ENV_FILE="${PHA_HOME}/envs/pha-22/pha-22.env"
+
 UID_VAR=$(id -u)
 GID_VAR=$(id -g)
 
-while getopts e: flag
+while getopts d:e:g: flag
 do
     case "${flag}" in
+        d) UID_VAR=${OPTARG};;
         e) ENV_FILE=${OPTARG};;
+        g) GID_VAR=${OPTARG};;
         *) error "Unexpected option ${flag}" ;;
     esac
 done
 
 set -a
-
 export UID_VAR=${UID_VAR}
 export GID_VAR=${GID_VAR}
 source ${ENV_FILE}
@@ -21,23 +23,23 @@ source ${ENV_FILE}
 docker build -t ${DOC_IMG}:${DOC_TAG} -f ${DOCKERFILE_PATH} \
         --build-arg="IMAGE_NAME=${IMAGE_NAME}" \
         --build-arg="IMAGE_VERSION=${IMAGE_VERSION}" \
-        --build-arg="NV_USERNAME=${NV_USERNAME}" \
+        --build-arg="IN_USERNAME=${IN_USERNAME}" \
         --build-arg="UID=${UID_VAR}" \
         --build-arg="GID=${GID_VAR}" \
-        --build-arg="NV_CUDA_VERSION_NUMBER=${NV_CUDA_VERSION_NUMBER}" \
-        --build-arg="NV_UBUNTU_VERSION=${NV_UBUNTU_VERSION}" \
-        --build-arg="NV_CUDNN_VERSION=${NV_CUDNN_VERSION}" \
-        --build-arg="NV_CUDNN_PACKAGE_NAME=${NV_CUDNN_PACKAGE_NAME}" \
-        --build-arg="NV_TRT=${NV_TRT}" \
+        --build-arg="IN_CUDA_VERSION_NUMBER=${IN_CUDA_VERSION_NUMBER}" \
+        --build-arg="IN_UBUNTU_VERSION=${IN_UBUNTU_VERSION}" \
+        --build-arg="IN_CUDNN_VERSION=${IN_CUDNN_VERSION}" \
+        --build-arg="IN_CUDNN_PACKAGE_NAME=${IN_CUDNN_PACKAGE_NAME}" \
+        --build-arg="IN_TRT=${IN_TRT}" \
         --build-arg="PROJECT_NAME=${PROJECT_NAME}" \
         --build-arg="APT_GET_REQUIREMENTS_FILE=${APT_GET_REQUIREMENTS_FILE}" \
         --build-arg="PYTHON_REQUIREMENTS_FILE=${PYTHON_REQUIREMENTS_FILE}" \
         --build-arg="SCRIPT_REQUIREMENTS_FILE=${SCRIPT_REQUIREMENTS_FILE}" \
-        --build-arg="NV_SSI_PATH=${NV_SSI_PATH}" \
-        --build-arg="NV_SCRIPTS_PATH=${NV_SCRIPTS_PATH}" \
-        --build-arg="NV_SOFTWARES_PATH=${NV_SOFTWARES_PATH}" \
+        --build-arg="IN_SSI_PATH=${IN_SSI_PATH}" \
+        --build-arg="IN_SCRIPTS_PATH=${IN_SCRIPTS_PATH}" \
+        --build-arg="IN_SOFTWARES_PATH=${IN_SOFTWARES_PATH}" \
         --build-arg="USER_REQUIREMENTS_PATH=${USER_REQUIREMENTS_PATH}" \
-        --build-arg="NV_ROS_VERSION=${NV_ROS_VERSION}" \
+        --build-arg="IN_ROS_VERSION=${IN_ROS_VERSION}" \
         --no-cache .
 
 set +a
